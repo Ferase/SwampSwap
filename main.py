a0d5c0ac5e765df45e1790596ab7ea4a31b0226c
Ferase <parkman202@gmail.com>
2026-09-13 00:56:58 -0500

    Incremeent version

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from app.workers.worker_croc import CrocWorker, CrocAction
 
 # Name and version variables
 _APP_NAME = "Swamp Swap"
-_APP_VERSION = "1.4.52"
+_APP_VERSION = "1.4.53"
 _MINIMUM_CROC_VERSION = "11.2.4"
 
 
